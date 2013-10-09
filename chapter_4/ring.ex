e613a4700a62b008e67a44815a2453a62552fcba
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2013-10-09 22:14:00 +0800

    completed ring exercise.

---
+++ chapter_4/ring.ex
@@ -0,0 +1,36 @@
+defmodule Ring do
+
+  def start(m, n, message) do
+    pid = spawn(__MODULE__, :start_process, [n])
+    pid <- {:message, message, m}
+  end
+
+  # Process 1
+  def start_process(count) do
+    pid = spawn(__MODULE__, :start_process, [count-1,  self])
+    loop(pid)
+  end
+
+  # Process N
+  def start_process(0, last) do
+    loop(last)
+  end
+
+  # The rest of the Processes
+  def start_process(count, last) do
+    pid = spawn(__MODULE__, :start_process, [count-1, last])
+    loop(pid)
+  end
+
+  def loop(next_pid) do
+    receive do
+      {:message, _, 0} -> 
+        true
+      {:message, message, m} -> 
+        IO.puts "m: #{m}. next_pid: #{inspect next_pid}"
+        next_pid <- {:message, message, m-1}
+        loop(next_pid)
+    end
+  end
+
+end
